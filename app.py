80556d407ef50bdf967211392160a2211c337b6b
yj7082126 <yk7da@virginia.edu>
2020-04-16 08:18:57 +0000

    edit errors

--- app.py
+++ app.py
@@ -14,22 +14,25 @@ slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/s
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
 #%%
-def send_arxiv(user_id, channel, categories = ["cs.CL", "cs.CV"], 
-               keywords = [], conferences = [], is_compact = False, 
+def send_arxiv(user_id, channel, categories = ["cs.CL", "cs.CV"],
+               keywords = [], conferences = [], is_compact = False,
                max_results = 5):
     arxivParser = ArxivParser(channel)
     if len(categories) == 0:
         categories = ["cs.CL", "cs.CV"]
+    else:
+        categories = categories
+
     if len(max_results) != 1:
         max_results = 5
     else:
         max_results = max_results[0]
-        
+
     arxivParser.parse_from_arxiv(categories, keywords, conferences)
     message = arxivParser.create_json(is_compact, max_results)
     response = slack_web_client.chat_postMessage(**message)
     assert response["ok"]
-    
+
 def send_help(user_id, channel):
     arxivParser = ArxivParser(channel)
     message = arxivParser.create_help_message()
@@ -43,21 +46,22 @@ def message(payload):
     channel_id = event.get("channel")
     user_id = event.get("user")
     text = event.get("text")
-
+    print(text)
     if text:
         text = text.split()
         if text[0] == "help":
             return send_help(user_id, channel_id)
         elif ((text[0] == "search") or (text[0] == "compact_search")):
             categories = [y for y in text[1:] if "." in y]
-            categories = [y for y in categories if y[re.search("\.", y).start()-1].islower() 
+            categories = [y.split('|')[1][:-1] if "|" in y else y for y in categories]
+            categories = [y for y in categories if y[re.search("\.", y).start()-1].islower()
                           and y[re.search("\.", y).start()+1].isupper()]
             keywords = [y for y in text[1:] if y.islower()]
             conferences = [y for y in text[1:] if y.isupper()]
             is_compact = False if text[0] == "search" else True
             max_results = [int(y) for y in text[1:] if y.isnumeric()]
-            
-            return send_arxiv(user_id, channel_id, categories, keywords, 
+
+            return send_arxiv(user_id, channel_id, categories, keywords,
                               conferences, is_compact, max_results)
 
 if __name__ == "__main__":
--- arxiv_parser.py
+++ arxiv_parser.py
@@ -6,14 +6,14 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 class ArxivParser:
-    
+
     def __init__(self, channel):
         self.channel = channel
         self.username = "arxiv_bot"
         self.timestamp = ""
-        
+
         self.base_url = "http://export.arxiv.org/api/query?search_query="
- 
+
     def create_help_message(self):
         blocks =  [
             {"type": "section",
@@ -24,15 +24,16 @@ class ArxivParser:
             },
             {"type": "divider"}
         ]
-        
+
         return {
             "ts": self.timestamp,
             "channel": self.channel,
             "username": self.username,
             "blocks": blocks,
         }
-    
+
     def parse_from_arxiv(self, search_categories, search_keywords, search_conferences):
+        print(search_categories)
         self.parse_url = self.base_url
         self.parse_url += ("%28" + "+OR+".join(["cat:" + x for x in search_categories]) + "%29")
         if len(search_keywords) > 0:
@@ -40,20 +41,20 @@ class ArxivParser:
             self.parse_url += ("%28" + "+OR+".join(["abs:" + x for x in search_keywords]) + "%29")
         if len(search_conferences) > 0:
             self.parse_url += "+AND+"
-            self.parse_url += ("%28" + "+OR+".join(["co:" + x for x in search_conferences]) + "%29")    
+            self.parse_url += ("%28" + "+OR+".join(["co:" + x for x in search_conferences]) + "%29")
         self.parse_url += "&start=0&max_results=50"
-        self.parse_url += "&sortBy=lastPublishedDate&sortOrder=descending"
-        
+        self.parse_url += "&sortBy=submittedDate&sortOrder=descending"
+
         print(self.parse_url)
         content = urlopen(self.parse_url)
         text = content.read().decode(content.headers.get_content_charset())
         soup = BeautifulSoup(text, "html.parser")
         self.info = soup.find_all('entry')
-        
+
     def create_json(self, is_compact = False, max_results = 5):
         divider_block = {"type":"divider"}
         blocks = [divider_block]
-            
+
         for i, val in enumerate(self.info):
             if i < max_results:
                 row, row2 = self.convert_value(val)
@@ -69,43 +70,43 @@ class ArxivParser:
             "username": self.username,
             "blocks": blocks,
         }
-    
+
     def parse_from_arxiv_df(self):
-        df = pd.DataFrame(columns = ["Id", "Title", "Updated", "Published", 
-                                     "Authors", "Categories", "Comment", "Summary"])        
+        df = pd.DataFrame(columns = ["Id", "Title", "Updated", "Published",
+                                     "Authors", "Categories", "Comment", "Summary"])
         for i, val in enumerate(self.info):
             df.loc[i] = self.convert_value_df(val)
-            
+
         return df
 
     def convert_value(self, val):
         result = dict()
         result2 = dict()
-        
+
         paper_title = "*<" + val.id.text + "|" + val.title.text.replace("\n", " ") + ">*\n"
         paper_comment = val.find_all('arxiv:comment')
         paper_comment = "" if len(paper_comment) == 0 else paper_comment[0].text.replace("\n", "")
         paper_title += paper_comment
-        
+
         paper_published = datetime.strptime(val.published.text, "%Y-%m-%dT%H:%M:%SZ")
         paper_published = int(time.mktime(paper_published.timetuple()))
         paper_published = "<!date^%d^Published {date_num} {time_secs}|Date unknown>"%(paper_published)
         paper_updated = datetime.strptime(val.updated.text, "%Y-%m-%dT%H:%M:%SZ")
         paper_updated = int(time.mktime(paper_updated.timetuple()))
-        paper_updated = "<!date^%d^Updated {date_num} {time_secs}|Date unknown>"%(paper_updated)        
+        paper_updated = "<!date^%d^Updated {date_num} {time_secs}|Date unknown>"%(paper_updated)
         paper_date = paper_published + "\n" + paper_updated
-        
+
         paper_authors = ", ".join([x.find('name').text for x in val.find_all('author')])
-        
+
         paper_categories = val.find_all('category')
         paper_categories = ", ".join([x['term'] for x in paper_categories])
-        
+
         paper_summary = val.summary.text
         paper_summary = ". ".join(paper_summary.split('. ')[:3]).replace("\n", " ").strip()
-        
+
         #is_accepted = len(re.findall("[A-Z][A-Z]", paper_comment)) > 0 or len(re.findall("Accepted|Submitted", paper_comment)) > 0
         is_accepted = True
-        
+
         if is_accepted:
             result['type'] = "section"
             result['text'] = {"type": "mrkdwn", "text": paper_title}
@@ -118,28 +119,28 @@ class ArxivParser:
                 {"type": "mrkdwn", "text": " "},
                 {"type": "mrkdwn", "text": paper_categories}
             ]
-            
+
             result2["type"] = "section"
             result2['text'] = {"type": "mrkdwn", "text": paper_summary}
-    
+
         return result, result2
-    
+
     def convert_value_df(self, val):
         paper_id = val.id.text
         paper_title = val.title.text
-        
+
         paper_updated = datetime.strptime(val.updated.text, "%Y-%m-%dT%H:%M:%SZ")
         paper_published = datetime.strptime(val.published.text, "%Y-%m-%dT%H:%M:%SZ")
-        
+
         paper_authors = [x.find('name').text for x in val.find_all('author')]
         paper_categories = val.find_all('category')
         paper_categories = ", ".join([x['term'] for x in paper_categories])
         paper_comment = val.find_all('arxiv:comment')
         paper_comment = "" if len(paper_comment) == 0 else paper_comment[0].text
-            
+
         paper_summary = val.summary.text
-        paper_summary = ". ".join(paper_summary.split('. ')[:3]).replace("\n", " ")   
-        
+        paper_summary = ". ".join(paper_summary.split('. ')[:3]).replace("\n", " ")
+
         row = [paper_id, paper_title, paper_updated, paper_published, paper_authors,
                paper_categories, paper_comment, paper_summary]
         return row
